ce497989f89b3a37dde897c20d3cb61d6c981122
kjirou <sorenariblog@gmail.com>
2018-01-16 21:34:52 +0900

    WIP: react-dnd検証中

--- src/App.js
+++ src/App.js
@@ -1,10 +1,77 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {DragDropContext, DragSource, DropTarget} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 import Select from 'react-select';
 
 import filterChangeEventDuringComposition from './lib/filterChangeEventDuringComposition';
 
 import 'react-select/dist/react-select.css'
 import './App.css';
+
+
+const Food = ({connectDragSource, foodName}) => {
+  return connectDragSource(
+    <li>{foodName}</li>
+  );
+};
+const DraggableFood = DragSource(
+  'Food',
+  {
+   beginDrag: (props) => {
+    return {
+      foodName: props.foodName,
+    };
+   },
+   endDrag: (props, monitor, component) => {
+    if (!monitor.didDrop()) {
+      return;
+    }
+    // TODO: 落としたところにドラッグした要素を挿入したいんだけどできてない
+    //       「ドラッグした要素の情報」はわかるけど、「どこに落としたのか」がわからない
+    console.log(monitor.getDropResult(), monitor.getItem());
+   }
+  },
+  (connect, monitor) => {
+    return {
+      connectDragSource: connect.dragSource(),
+    };
+  }
+)(Food);
+const FoodList = ({connectDropTarget, foodNames}) => {
+  return connectDropTarget(
+    <ul>
+    {
+      foodNames.map((foodName, index) => {
+        return <DraggableFood key={index} foodName={foodName} />;
+      })
+    }
+    </ul>
+  );
+};
+const DroppableFoodList = DropTarget(
+  ['Food'],
+  {
+    drop(props, monitor, component) {
+      return {
+        targetProps: props,
+      };
+    },
+  },
+  (connect, monitor) => {
+    return {
+      connectDropTarget: connect.dropTarget(),
+    };
+  }
+)(FoodList);
+
+class DndSection extends Component {
+  render() {
+    return <div>
+      <DroppableFoodList foodNames={['Apple', 'Orange', 'Grape']} />
+    </div>;
+  }
+}
+const DndSection2 = DragDropContext(HTML5Backend)(DndSection);
 
 
 class App extends Component {
@@ -22,8 +89,10 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h2>react-dnd</h2>
 
         <h2>react-select</h2>
+        <DndSection2 />
 
         <h3>Basic</h3>
         <div style={{width: 200}}>
